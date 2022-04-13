61b6933fd767acdedb26b6206f27d2b7995d3fee
unknown <kumarabhishek0947@gmail.comgit config --global user.name AAbhishek18>
2022-04-13 08:50:14 +0530

    12th APR Asshinment Abhsihek

--- src/routes/route.js
+++ src/routes/route.js
@@ -8,12 +8,26 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+//router.post("/createUser", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
+//router.get("/getUsersData", UserController.getUsersData)
 
+// 1st  API
 router.post("/createBook", BookController.createBook  )
 
-router.get("/getBooksData", BookController.getBooksData)
+// 2nd API for book list
+router.get("/bookList", BookController.bookList  )
+
+// 3RD API
+router.get("/getBooksInYear", BookController.getBooksInYear  )
+
+// 4TH API
+router.get("/getParticularBooks", BookController.getParticularBooks  )
+
+// 5th API
+router.get("/getXINRBooks", BookController.getXINRBooks)
+
+// 6TH API
+router.get("/getRandomBooks", BookController.getRandomBooks)
 
 module.exports = router;
